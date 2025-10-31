42c2d32bcfb875f323094464692ec37b7a0d2f22
iluvshiwoon <sntcillian@gmail.com>
2025-10-31 17:35:05 +0100

    enable python lsp + linter

--- shared/config/neovim/lua/config/lsp.lua
+++ shared/config/neovim/lua/config/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable({ "nixd", "luals", "clangd", "svelte", "tsserver", "tailwindcss" })
+vim.lsp.enable({ "nixd", "luals", "clangd", "svelte", "tsserver", "tailwindcss", "ruff", "basedpyright" })
 vim.diagnostic.config({
 	virtual_text = { current_line = true },
 })
