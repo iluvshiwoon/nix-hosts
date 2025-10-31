b2150642ba4318a96d79c588671076adfb4d614e
iluvshiwoon <sntcillian@gmail.com>
2025-10-31 15:31:02 -0400

    fix position encoding issue as pyright use utf 16

--- shared/config/neovim/lsp/basedpyright.lua
+++ shared/config/neovim/lsp/basedpyright.lua
@@ -1,3 +1,4 @@
+-- neovim/lsp/basedpyright.lua
 return {
 	cmd = { "basedpyright-langserver", "--stdio" },
 	filetypes = { "python" },
@@ -5,10 +6,8 @@ return {
 	settings = {
 		python = {
 			analysis = {
-				-- These help find your venv and libraries
 				autoSearchPaths = true,
 				useLibraryCodeForTypes = true,
-				-- This tells it to analyze the whole project, not just open files
 				diagnosticMode = "workspace",
 			},
 		},
