09d09d5d2604714c4ef8a8a9e689f7069487ccc1
Killian Griset <kgriset@f2r5s15.clusters.42paris.fr>
2025-12-11 22:10:27 +0100

    add school script

---
+++ script/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export NIXPKGS_ALLOW_UNFREE=1
+export NP_LOCATION=/media/$USER/Nix_Store
+export NP_DEBUG=1
+export NP_GIT=/usr/bin/git
+export NP_RUNTIME=bwrap
+
+NIX=$HOME/nix-portable
+
+export TERM=xterm-256color
+export PATH=$HOME/.nix-profile/bin:$PATH
+
+#fix double char
+export LANG=C.UTF-8
+
+$NIX nix-shell -p nix --command "bash"
